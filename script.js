82e0e850f8bad1172383f5311d6fa53e91bb8148
Zain0-0 <122361692+Zain0-0@users.noreply.github.com>
2024-03-16 10:29:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSideBar(){
+    const sideBar = document.querySelector('.sideBar')
+    sideBar.style.display = 'flex'
+}
+function closeSideBar(){
+    const sideBar =document.querySelector(".sideBar")
+    sideBar.style.display="none"
+}
